d1f7c0893d0c1239e38eabfdbb0379815b8eeda4
WALID_TOUMI <raja12rr1234@gmail.com>
2026-08-03 19:39:20 +0100

    **

--- src/main/java/com/gestion_de_note/gestion_de_note/secryte/Userdetailimpli.java
+++ src/main/java/com/gestion_de_note/gestion_de_note/secryte/Userdetailimpli.java
@@ -1,5 +1,35 @@
 package com.gestion_de_note.gestion_de_note.secryte;
 
-public class Userdetailimpli {
+import org.springframework.security.core.GrantedAuthority;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class Userdetailimpli implements Userdetails{
     
+
+
+     private static final long serialVersionUID = 1L
+
+
+      private  long id ;
+      private String ueraname ;
+      private String email;
+
+      @JsonIgnore 
+
+       private String password ;
+
+       Private Collection<? extends GrantedAuthority> authorities;
+ 
+ 
+       public Userdetailimpli( long id , String username, String email , String passsword, Collevtion<? extends GrantedAuthrity> autthorities){
+
+
+            this.id = id ;
+            this.useraname = username ;
+            this.email = email ;
+            this.passwoerd = password ;
+            thisa.uthorties = authorties ;
+ }
+
 }
